afb58cdee5738cbc4e7af2ae21c1955e4ea4301f
marko <salamanka1985@gmail.com>
2019-10-25 18:24:23 +0200

    Initial commit

---
+++ src/main/java/com/petclinic/SpringGuruPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuruPetclinicApplication.class, args);
+	}
+
+}
