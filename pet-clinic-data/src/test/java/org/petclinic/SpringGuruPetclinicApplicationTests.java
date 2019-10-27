fc4db350dfb331a3358fd825b5ea104022b1d6d5
marko <salamanka1985@gmail.com>
2019-10-27 21:06:40 +0100

    refactoring to multimodule maven project. closes #1

---
+++ pet-clinic-data/src/test/java/org/petclinic/SpringGuruPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuruPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
