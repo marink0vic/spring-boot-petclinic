757df818fe380067b6010e0aea6e9ed31a951966
marko <salamanka1985@gmail.com>
2019-11-06 19:06:02 +0100

    Create spring data jpa service for owner

--- pet-clinic-data/src/main/java/com/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/petclinic/repositories/OwnerRepository.java
@@ -7,5 +7,6 @@ import com.petclinic.model.Owner;
 
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+	
+	Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/com/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,64 @@
+package com.petclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.petclinic.model.Owner;
+import com.petclinic.repositories.OwnerRepository;
+import com.petclinic.repositories.PetRepository;
+import com.petclinic.services.OwnerService;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+	private final OwnerRepository ownerRepository;
+
+	@Autowired
+	public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Iterable<Owner> ownersIterable = ownerRepository.findAll();
+		Set<Owner> owners = new HashSet<>();
+		ownersIterable.forEach(owners::add);
+		
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		Optional<Owner> optionalOwner = ownerRepository.findById(id);
+		
+		return optionalOwner.orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner owner) {
+		return ownerRepository.save(owner);
+	}
+
+	@Override
+	public void delete(Owner owner) {
+		ownerRepository.delete(owner);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownerRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		Owner owner = ownerRepository.findByLastName(lastName);
+		
+		return owner;
+	}
+}
